5817573280aa441fa0d5b5cf7dcd6cfe87435214
R. Vrzdiak <rv@profioss.com>
2019-10-07 00:24:15 +0200

    get-wiki-index-components - DJIA wiki changed, parsing updated

--- cmd/get-wiki-index-components/djia.go
+++ cmd/get-wiki-index-components/djia.go
@@ -33,8 +33,9 @@ func parseDJIA(r io.Reader) (output [][]string, err error) {
 				switch {
 				case thTd != nil && thTd.Data == "td":
 					link := thTd.FirstChild
-					if link.Type == html.ElementNode && link.Data == "a" {
-						row = append(row, strings.TrimSpace(link.FirstChild.Data))
+					if link != nil && link.Type == html.ElementNode && link.Data == "a" {
+						// extract td element
+						row = append(row, extractTextDJIA(thTd))
 					}
 				case thTd != nil && thTd.Data == "th":
 					// process th if needed
@@ -52,7 +53,7 @@ func parseDJIA(r io.Reader) (output [][]string, err error) {
 	f(doc)
 
 	for _, r := range rows {
-		company := r[0]
+		company := strings.TrimSpace(r[0])
 		// exchange := r[1]
 		ticker := r[2]
 		row := []string{ticker, company}
@@ -60,4 +61,26 @@ func parseDJIA(r io.Reader) (output [][]string, err error) {
 	}
 
 	return output, nil
+}
+
+func extractTextDJIA(n *html.Node) string {
+	output := ""
+
+	link := n.FirstChild
+	if link.Type == html.ElementNode && link.Data == "a" {
+		output += strings.TrimSpace(link.FirstChild.Data)
+	}
+
+	// Symbol column can contain for example: NYSE: MMM
+	// detect colon and extract text from following link
+	if link.NextSibling != nil && link.NextSibling.Data == ":\u00a0" {
+		// link after colon
+		link2 := link.NextSibling.NextSibling
+		if link2 != nil && link2.Type == html.ElementNode && link2.Data == "a" {
+			// return only ticker which is relevant data
+			return strings.TrimSpace(link2.FirstChild.Data)
+		}
+	}
+
+	return output
 }
